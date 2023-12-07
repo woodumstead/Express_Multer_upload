ba304d288ee15df9dbc08764b8b534542da49fda
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-12-07 12:44:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+//----- Express import
+const express = require('express');
+const app = express();
+
+
+//------ Multer import
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+//----- get route
+app.get('/', (req, res) => {
+    res.send('');
+}); 
+
+//------ post route -------   w/upload.single middleware
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    res.json(req.file);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+});
